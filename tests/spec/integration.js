83f3d7894887af47130a7dcfe20236cc3ab654d3
kylem038 <misenckj@gmail.com>
2016-08-23 18:41:24 -0600

    Broke files off of index.js and added integration tests

--- tests/spec/integration.js
+++ tests/spec/integration.js
@@ -1,9 +1,34 @@
-const assert =  require('assert')
+const assert =  require('assert');
 
 describe('welcome page', function(){
   it('should be able to grab the page title', function(){
     browser.url('/');
-    var title = browser.getTitle()
-    assert.equal(title, 'hello world');
+    var title = browser.getTitle();
+    assert.equal(title, 'Kyle Misencik & Ben Godfrey - Tier 1 Turing');
+  });
+
+  it('should have an input field for work time', function() {
+    let workInput = browser.element('.work-input');
+
+    workInput.setValue('25:00');
+
+    assert.equal(workInput.getValue(), '25:00');
+  });
+
+  it('should have an input field for break time', function() {
+    let breakInput = browser.element('.break-input');
+
+    breakInput.setValue('5:00');
+
+    assert.equal(breakInput.getValue(), '5:00');
+  });
+
+  it.skip('should have a button to start the timer', function() {
+    browser.url('/');
+    let startButton = browser.element('.start-timer');
+
+    browser.click(startButton);
+
+    assert.equal();
   });
 });
