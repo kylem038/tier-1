fe56f0ad15ad623665e1f44e42af449a47d3bc85
kylem038 <misenckj@gmail.com>
2016-08-26 17:40:18 -0600

    Got unit tests in for start, end, and default values

--- tests/unit/index-test.js
+++ tests/unit/index-test.js
@@ -1,4 +1,4 @@
-const assert = require('chai').assert
+const assert = require('chai').assert;
 const Clock = require('../../lib/clock');
 
 describe('our test bundle', function () {
@@ -9,19 +9,47 @@ describe('our test bundle', function () {
 
 describe('our clock', function () {
   it('is an object', function () {
-    var clock = new Clock();
+    let clock = new Clock();
     assert.isObject(clock);
   });
 
-  it('should have a function that countDown', function () {
-    var clock = new Clock();
-    var seconds = 10;
-    clock.countDown(seconds);
-    //assert that the seconds reaches 0
-    assert.equal();
-    //assert that the seconds doesn't hit -1
-    assert.equal();
+  it('should take the first argument and set it as the duration', function(){
+    let clock = new Clock(10);
+    assert.equal(clock.duration, 10);
   });
+
+  it('should have a default start time equal to null', function(){
+    let clock = new Clock();
+    assert.equal(clock.startTime, null);
+  });
+
+  it('should have a default  endTime equal to null', function(){
+    let clock = new Clock();
+    assert.equal(clock.endTime, null);
+  });
+
+  it('should have a function that starts the clock', function () {
+    let clock = new Clock();
+    let time = Date.now();
+    clock.start(time);
+    assert.equal(clock.startTime, time);
+  });
+
+  it('should have a function to find the endTime', function() {
+    let clock = new Clock(10);
+    let time = Date.now();
+    clock.start(time);
+    assert.equal(clock.endTime, (time+10));
+  });
+
+  it('should have a function to find timeInBetween', function() {
+    let clock = new Clock(10);
+    let time = Date.now();
+    clock.start(time);
+    clock.endTime();
+    assert.equal(clock.timeInBetween, (clock.endTime - time));
+  });
+
 
 
 });
