ecb8ddc7d3c2cdaf04fb9af347c7774d97f8f4f5
kylem038 <misenckj@gmail.com>
2016-08-29 14:37:04 -0600

    Fixed tests and worked on responsive styles

--- webpack.config.js
+++ webpack.config.js
@@ -13,9 +13,15 @@ module.exports = {
    loaders: [
      { test: /\.css$/, loader: "style!css" },
      { test: /\.scss$/, loader: "style!css!sass" }
+     { test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
    ]
   },
   resolve: {
     extensions: ['', '.js', '.json', '.scss', '.css']
-  } 
+  }
 }
